8a5ae6af15bd7ea8e3251ef49228bc5124c94c92
Pooja Rao <raopooj@gmail.com>
2021-10-06 13:37:28 +1100

    Adding features of weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
+import axios from 'axios'
+import TextField from "@mui/material/TextField"
+import Button from "@mui/material/Button"
 
 function App() {
+  const [search, setSearch] = useState("")
+  const [weatherData, setWeatherData] = useState({})
+  console.log(process.env.REACT_APP_WEATHER_API)
+   useEffect(() => {
+  
+   }, [])
+
+  const fetchData = async () => {
+      // TODO: handle errors in seach more gracefully
+      const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?appid=${process.env.REACT_APP_WEATHER_API}&units=metric&q=${search}`)
+      if (data.weather) setWeatherData(data)
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header></header>
+      <main>
+        <div>
+        <TextField id="outlined-basic" label="City" variant="outlined" value={search} onChange={(e) => setSearch(e.target.value)} />
+        <Button variant="outlined" onClick={fetchData}>Search</Button>
+        </div>
+        
+        {weatherData.weather && (
+          <>
+          <img src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`
+} alt={weatherData.weather[0].description} />
+          <h1>{weatherData.main.temp}°C</h1>
+             <h2>
+              {weatherData.name}, {weatherData.sys.country}
+            </h2>
+          </>
+        )}
+        {/* <Icon />
+        <CurrentTemp />
+        <div className="flex">
+          <MinTemp />
+          <MaxTemp />
+        </div> */}
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
